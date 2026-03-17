1fe530e5fdba683f2a1254fb979943177f46aad7
contactoaplicacionesmovil-lgtm <contacto.aplicaciones.movil@gmail.com>
2026-03-16 21:38:53 -0500

    new secure auth spring proj

---
+++ SecureSpringApp/src/main/java/com/example/securespringapp/SecureSpringAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.securespringapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureSpringAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureSpringAppApplication.class, args);
+    }
+
+}
---
+++ SecureSpringApp/src/test/java/com/example/securespringapp/SecureSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securespringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
