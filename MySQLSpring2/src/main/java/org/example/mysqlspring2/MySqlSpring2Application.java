ee1bcbfc1ca3a815fededd79653caf5ac821167b
contactoaplicacionesmovil-lgtm <contacto.aplicaciones.movil@gmail.com>
2026-03-25 20:16:08 -0500

    using jpa repository and entity anotation

---
+++ MySQLSpring2/src/main/java/org/example/mysqlspring2/MySqlSpring2Application.java
@@ -0,0 +1,13 @@
+package org.example.mysqlspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySqlSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySqlSpring2Application.class, args);
+    }
+
+}
